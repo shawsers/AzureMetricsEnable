8e8df584d226757693707809f16ce6311a82afe0
shawsers <53303655+shawsers@users.noreply.github.com>
2019-07-26 12:18:38 -0400

    removed commented out lines

--- AzureCreateStorageAccount.ps1
+++ AzureCreateStorageAccount.ps1
@@ -20,19 +20,3 @@ $TimeStamp = Get-Date -Format o | foreach {$_ -replace ":", "."}
 $sub = login-azurermaccount -Subscription $subscriptionId
 $newresgroup = New-AzurermResourceGroup -Name $resourcegroup -Location $location
 $storageAccount = New-AzurermStorageAccount -ResourceGroupName $resourcegroup -Name $storageaccountname -Location $location -Kind StorageV2 -SkuName Standard_LRS
-
-#$region = get-azurermlocation | Select-Object Location
-#$region.location | out-file -FilePath .\locations.txt
-#$data = get-content .\locations.txt
-#$question = Read-Host 'This will create a storage account per region, type YES to continue'
-#if ($question = "YES"){
-#foreach ($reg in $data){
-#    New-AzurermStorageAccount -ResourceGroupName $resourcegroup -Name DIAGNOSTICS$reg -Location $reg -SkuName Standard_LRS -Kind StorageV2 | out-file -filepath .\$timestamp.txt -Append
-#    }}
-#else{
-#Write-Host "Script cancelled, as input was not YES to continue" -ForegroundColor Green
-#}
-##$stgacct = Get-AzurermStorageAccount | ? { $_.StorageAccountName -match $storageaccountname }
-##$sub | out-file -filepath .\$timestamp.txt -Append
-##$stgacct | out-file -filepath .\$timestamp.txt -Append
-##$error | out-file -filepath .\$timestamp.txt -Append
