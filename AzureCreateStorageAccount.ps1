40f2d5945559d845f5bae2ad70ccba468f6f8439
shawsers <53303655+shawsers@users.noreply.github.com>
2019-07-26 11:50:08 -0400

    updated

--- AzureCreateStorageAccount.ps1
+++ AzureCreateStorageAccount.ps1
@@ -1,4 +1,6 @@
-#Create new resource group and storage account in Azure
+#The script will create a new Azure storage account in the resource group, location and subscription specified in the parameters
+
+#example: .\AzureCreateStorageAccount.ps1 -subscriptionid SUB-ID-HERE -location AZURE-LOCATION -resroucegroup NEW-RES-GROUP-NAME - storageaccount NEW-DIAG-STORAGE
 
 param(
  [Parameter(Mandatory=$True)]
@@ -10,20 +12,14 @@ param(
  [Parameter(Mandatory=$True)]
  [string] $resourcegroup,
 
- [Parameter(Mandatory=$False)]
- [string] $vmname,
-
  [Parameter(Mandatory=$True)]
  [string] $storageaccountname,
-
- [Parameter(Mandatory=$True)]
- [string] $storagetype
 )
 $error.clear()
 $TimeStamp = Get-Date -Format o | foreach {$_ -replace ":", "."}
 $sub = login-azurermaccount -Subscription $subscriptionId
 $newresgroup = New-AzurermResourceGroup -Name $resourcegroup -Location $location
-$storageAccount = New-AzurermStorageAccount -ResourceGroupName $resourcegroup -Name $storageaccountname -Location $location -SkuName Standard_LRS
+$storageAccount = New-AzurermStorageAccount -ResourceGroupName $resourcegroup -Name $storageaccountname -Location $location -Kind StorageV2 -SkuName Standard_LRS
 
 #$region = get-azurermlocation | Select-Object Location
 #$region.location | out-file -FilePath .\locations.txt
