eaaa63aa2a5e8ba1f76d66a8e5e137854c3193b6
shawsers <53303655+shawsers@users.noreply.github.com>
2019-07-26 11:45:23 -0400

    updated

--- AzureCreateStorageAccount.ps1
+++ AzureCreateStorageAccount.ps1
@@ -4,7 +4,7 @@ param(
  [Parameter(Mandatory=$True)]
  [string] $subscriptionId,
 
- [Parameter(Mandatory=$False)]
+ [Parameter(Mandatory=$True)]
  [string] $location,
 
  [Parameter(Mandatory=$True)]
@@ -22,19 +22,20 @@ param(
 $error.clear()
 $TimeStamp = Get-Date -Format o | foreach {$_ -replace ":", "."}
 $sub = login-azurermaccount -Subscription $subscriptionId
-$region = get-azurermlocation | Select-Object Location
-$region.location | out-file -FilePath .\locations.txt
-$data = get-content .\locations.txt
-$question = Read-Host 'This will create a storage account per region, type YES to continue'
-if ($question = "YES"){
-foreach ($reg in $data){
-    New-AzurermStorageAccount -ResourceGroupName $resourcegroup -Name DIAGNOSTICS$reg -Location $reg -SkuName Standard_LRS -Kind StorageV2 | out-file -filepath .\$timestamp.txt -Append
-    }}
-else{
-Write-Host "Script cancelled, as input was not YES to continue" -ForegroundColor Green
-}
-##$newresgroup = New-AzurermResourceGroup -Name $resourcegroup -Location $location
-##$storageAccount = New-AzurermStorageAccount -ResourceGroupName $resourcegroup -Name $storageaccountname -Location $location -SkuName Standard_LRS
+$newresgroup = New-AzurermResourceGroup -Name $resourcegroup -Location $location
+$storageAccount = New-AzurermStorageAccount -ResourceGroupName $resourcegroup -Name $storageaccountname -Location $location -SkuName Standard_LRS
+
+#$region = get-azurermlocation | Select-Object Location
+#$region.location | out-file -FilePath .\locations.txt
+#$data = get-content .\locations.txt
+#$question = Read-Host 'This will create a storage account per region, type YES to continue'
+#if ($question = "YES"){
+#foreach ($reg in $data){
+#    New-AzurermStorageAccount -ResourceGroupName $resourcegroup -Name DIAGNOSTICS$reg -Location $reg -SkuName Standard_LRS -Kind StorageV2 | out-file -filepath .\$timestamp.txt -Append
+#    }}
+#else{
+#Write-Host "Script cancelled, as input was not YES to continue" -ForegroundColor Green
+#}
 ##$stgacct = Get-AzurermStorageAccount | ? { $_.StorageAccountName -match $storageaccountname }
 ##$sub | out-file -filepath .\$timestamp.txt -Append
 ##$stgacct | out-file -filepath .\$timestamp.txt -Append
