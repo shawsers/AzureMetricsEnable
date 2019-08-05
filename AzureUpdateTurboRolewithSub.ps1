3be42eca84a418bdc44224572ac86e02d833c099
shawsers <jasonshaw@J-MacBook-Pro.local>
2019-08-05 10:29:51 -0400

    initial

---
+++ AzureUpdateTurboRolewithSub.ps1
@@ -0,0 +1,21 @@
+#Search Azure subscriptions for existing Turbonomic custom role
+$turboRoleSubscription = $null
+foreach($azureSubscription in Get-AzureRmSubscription){
+ 
+    #Set the script's focus one subscription at a time
+    Select-AzureRmSubscription -Subscription $azureSubscription.Id
+ 
+    #If the Azure custom role is already created, capture the subscription ID
+    if(Get-AzureRmRoleDefinition | Where-Object{$_.Name -like '*Turbonomic*'}){
+        $turboRoleSubscription = $azureSubscription
+    }
+ 
+}
+ 
+#if an existing custom role is found, update the role's scope with target subscription info
+if($turboRoleSubscription){
+    Select-AzureRmSubscription -Subscription $azureSubscription.Id
+    $turboCustomRole = Get-AzureRmRoleDefinition | Where-Object{$_.Name -like '*Turbonomic*'}
+    $turboCustomRole.AssignableScopes.Add($targetSubID)
+ 
+}
