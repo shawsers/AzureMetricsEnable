86d18e8d015472fda6f01eb1ef004a8c815146b7
shawsers <jasonshaw@J-MacBook-Pro.local>
2019-08-06 10:55:07 -0400

    updated to read only

--- AzureCreateTurboRole.ps1
+++ AzureCreateTurboRole.ps1
@@ -14,12 +14,13 @@ $role = Get-AzureRmRoleDefinition -Name "Virtual Machine Contributor"
 $role.Id = $null
 $role.Name = "Turbonomic Read Memory Metrics Role"
 $role.Description = "Turbonomic Permissions required to read memory metrics"
+$role.Actions.Clear()
 $role.Actions.Add("*/read")
-$role.Actions.Add("Microsoft.Compute/virtualMachines/write")
-$role.Actions.Add("Microsoft.Compute/virtualMachines/start/action")
-$role.Actions.Add("Microsoft.Compute/virtualMachines/deallocate/action")
-$role.Actions.Add("Microsoft.Compute/disks/write")
-$role.Actions.Add("Microsoft.Network/networkInterfaces/join/action")
+#$role.Actions.Add("Microsoft.Compute/virtualMachines/write")
+#$role.Actions.Add("Microsoft.Compute/virtualMachines/start/action")
+#$role.Actions.Add("Microsoft.Compute/virtualMachines/deallocate/action")
+#$role.Actions.Add("Microsoft.Compute/disks/write")
+#$role.Actions.Add("Microsoft.Network/networkInterfaces/join/action")
 $role.AssignableScopes.Clear()
 $role.AssignableScopes.Add("/subscriptions/$subscriptionid")
 $role.AssignableScopes.Add("/subscriptions/$subscriptionid/resourceGroups/$resourceGroup/providers/Microsoft.Storage/storageAccounts/$storageaccount")
