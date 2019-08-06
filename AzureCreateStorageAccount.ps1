e6b6195693970b6ec245a7395b367ad43fe1ad9e
shawsers <jasonshaw@J-MacBook-Pro.local>
2019-08-06 08:41:43 -0400

    updated

--- AzureCreateStorageAccount.ps1
+++ AzureCreateStorageAccount.ps1
@@ -17,7 +17,12 @@ param(
 )
 $error.clear()
 $TimeStamp = Get-Date -Format o | foreach {$_ -replace ":", "."}
-$sub = login-azurermaccount -Subscription $subscriptionId
+#Login Azure Account
+$sub = login-azurermaccount -Subscription $subscriptionId -ErrorAction Stop
+
+#Create new Resource Group for the new Stoage Account
 $newresgroup = New-AzurermResourceGroup -Name $resourcegroup -Location $location
+
+#Create new Storage Account for metrics
 $storageAccount = New-AzurermStorageAccount -ResourceGroupName $resourcegroup -Name $storageaccountname -Location $location -Kind StorageV2 -SkuName Standard_LRS
 
