1e82436b786d4b541766abef28eea47ab5edfdd4
shawsers <jasonshaw@J-MacBook-Pro.local>
2019-08-06 10:17:03 -0400

    updated logging and loop

--- AzureCreateStorageAccount.ps1
+++ AzureCreateStorageAccount.ps1
@@ -1,6 +1,6 @@
 #The script will create a new Azure storage account in the resource group, location and subscription specified in the parameters
 
-#example: .\AzureCreateStorageAccount.ps1 -subscriptionid SUB-ID-HERE -location AZURE-LOCATION -resroucegroup NEW-RES-GROUP-NAME - storageaccount NEW-DIAG-STORAGE
+#example: .\AzureCreateStorageAccount.ps1 -subscriptionid SUB-ID-HERE -location AZURE-LOCATION -resourcegroup NEW-RES-GROUP-NAME - storageaccount NEW-DIAG-STORAGE
 
 param(
  [Parameter(Mandatory=$True)]
@@ -13,16 +13,28 @@ param(
  [string] $resourcegroup,
 
  [Parameter(Mandatory=$True)]
- [string] $storageaccountname
+ [string] $storageaccount
 )
 $error.clear()
 $TimeStamp = Get-Date -Format o | foreach {$_ -replace ":", "."}
 #Login Azure Account
-$sub = login-azurermaccount -Subscription $subscriptionId -ErrorAction Stop
-
+login-azurermaccount -Subscription $subscriptionId -ErrorAction Stop
+$sub = Get-AzureRmSubscription 
+$subname = $sub.Name
 #Create new Resource Group for the new Stoage Account
 $newresgroup = New-AzurermResourceGroup -Name $resourcegroup -Location $location
 
-#Create new Storage Account for metrics
-$storageAccount = New-AzurermStorageAccount -ResourceGroupName $resourcegroup -Name $storageaccountname -Location $location -Kind StorageV2 -SkuName Standard_LRS
+#Get List of VM's locations
+$vmsloc = get-azurermvm | Select-Object -Unique -ExpandProperty "Location"
 
+Add-Content -Path .\ResandStorage.csv -Value "Subscription Name,Subscription ID,Resource Group,Storage Account,Storage Location"
+#Add foreach loop for creating storage account per $vmsloc variable
+$count = 0
+foreach($storloc in $vmsloc){
+$count++
+$storageaccountname = $storageaccount + $count
+#Create new Storage Account for metrics
+New-AzurermStorageAccount -ResourceGroupName $resourcegroup -Name $storageaccountname -Location $location -Kind StorageV2 -SkuName Standard_LRS
+
+Add-Content -Path .\ResandStorage.csv -Value "$subname,$subscriptionId,$resourcegroup,$storageaccountname,$storloc"
+}
--- AzureUpdateTurboRolewithSub.ps1
+++ AzureUpdateTurboRolewithSub.ps1
@@ -1,5 +1,6 @@
 #Search Azure subscriptions for existing Turbonomic custom role
 $turboRoleSubscription = $null
+Add-Content -Path .\TurboRoleAddedToSubScope.csv -Value "Subscription Name,Subscription ID,Turbo Custom Role Name"
 foreach($azureSubscription in Get-AzureRmSubscription){
  
     #Set the script's focus one subscription at a time
@@ -15,7 +16,6 @@ foreach($azureSubscription in Get-AzureRmSubscription){
             $turboCustomRole = Get-AzureRmRoleDefinition | Where-Object{$_.Name -like '*Turbonomic*'}
             $turboCustomRole.AssignableScopes.Add("/subscriptions/$targetSubID")
             $turboCustomRoleName = $turboCustomRole.Name
-            Add-Content -Path .\TurboRoleAddedToSubScope.csv -Value "Subscription Name,Subscription ID,Turbo Custom Role Name"
             Add-Content -Path .\TurboRoleAddedToSubScope.csv -Value "$subname,$targetSubID,$TurboCustomRoleName"
         }
     }
