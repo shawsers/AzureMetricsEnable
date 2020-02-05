3df9f437dd6377b49647c679c7e8f0ca63610526
shawsers <53303655+shawsers@users.noreply.github.com>
2020-02-05 09:33:10 -0500

    Create AuditRGContribRole.ps1

---
+++ AuditRGContribRole.ps1
@@ -0,0 +1,27 @@
+#START SCRIPT
+#Make sure to create a file named subs.txt and add all of the sub names to it you want to run the script on
+connect-azurermaccount -ErrorAction Stop
+$date = date
+Write-Host "Script started at: $date"
+Write-Host "Reading input file"
+$subs = Get-Content -Path .\subs.txt
+Add-Content -Path .\ContributorRole-RGs.csv -Value "Sub Name,RG Name,Role Name,Display Name,Sign In Name,Object Type"
+foreach ($sub in $subs){
+    Write-host "Starting sub name: $sub"
+    $selectsub = Select-AzureRmSubscription -Subscription $sub
+    write-host "Getting list of RGs in the sub"
+    $allrgs = get-azurermresourcegroup
+    foreach ($rg in $allrgs){
+        $rgname = $rg.ResourceGroupName
+        Write-host "Getting all accounts with Contributor role on the RG named $rgname"
+        $contrib = $rg|get-azurermroleassignment|where{$_.RoleDefinitionName -eq "Contributor"}
+        $diaplayname = $contrib.DisplayName
+        $signinname = $contrib.SignInName
+        $role = $contrib.RoleDefinitionName
+        $objecttype = $contrib.ObjectType
+        Write-host "Output results to output file"
+        Add-Content -Path .\ContributorRole-RGs.csv -Value "$sub,$rgname,$role,$displayname,$signinname,$objecttype"
+    }
+}
+Write-host "Script is complete please review output file ContributorRole-RGs.csv"
+#END SCRIPT
